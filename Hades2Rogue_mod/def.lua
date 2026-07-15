58ea2da5809e20a4f436212eb7cc3f212fa78f65
BrittisH39 <brittonboy9@gmail.com>
2026-07-15 10:57:00 -0600

    Delete Hades2Rogue_mod directory

--- Hades2Rogue_mod/def.lua
+++
@@ -1,7 +0,0 @@
----@meta BrittisH39-Hades2RogueArchipelago
-local public = {}
-
--- Public surface other plugins could use. Currently only the config is exposed
--- (see main.lua: `public.config = config`).
-
-return public
